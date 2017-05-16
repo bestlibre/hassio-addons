280e78ef4348794e8b0f07e46e93de5716eb1a3a
BestLibre <github@bestlibre.org>
2017-05-16 16:46:07 +0200

    Adding influxdb to build

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 ./create_hassio_addon.sh -a armhf -s mopidy -p -l .
 ./create_hassio_addon.sh -a amd64 -s mopidy -p -l .
+./create_hassio_addon.sh -a armhf -s influxdb -p -l .
+./create_hassio_addon.sh -a amd64 -s influxdb -p -l .
