f0df0fea561312923703b049caecd627c79ac05b
bestlibre <github@bestlibre.org>
2017-05-16 20:13:01 +0200

    fix run.sh for mopidy

--- mopidy/run.sh
+++ mopidy/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-options=cat /data/options.json | jq -r '[.options[] | "-o "+.name+"="+.value ] | join(" ")'
+options=$(catcat /data/options.json | jq -r '[.options[] | "-o "+.name+"="+.value ] | join(" ")')
 
 mopidy --config /var/lib/mopidy/.config/mopidy/mopidy.conf $options
