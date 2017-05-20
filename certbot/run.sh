bbcc5cc0a98df9f3d7dd1d68d96f862dfeb5db64
BestLibre <github@bestlibre.org>
2017-05-20 16:27:30 +0200

    one certificate per domain

--- certbot/run.sh
+++ certbot/run.sh
@@ -9,21 +9,17 @@ EMAIL=$(jq --raw-output ".email" $CONFIG_PATH)
 DOMAINS=$(jq --raw-output ".domains[]" $CONFIG_PATH)
 DEBUG=$(jq --raw-output ".debug // empty" $CONFIG_PATH)
 
-for line in $DOMAINS; do
-    if [ -z "$DOMAIN_ARG" ]; then
-        DOMAIN_ARG="-d $line"
-    else
-        DOMAIN_ARG="$DOMAIN_ARG -d $line"
-    fi
-done
-
 mkdir -p /ssl/wk
 
 while true; do
-    certbot certonly --webroot -w /ssl/wk/ --non-interactive --email "$EMAIL" --agree-tos --config-dir "$CERT_DIR" --work-dir "$WORK_DIR" ${DOMAIN_ARG}
-    if [ "$DEBUG" == "true" ]; then
-	cat /var/log/letsencrypt/letsencrypt.log
-    fi
+
+    for DOMAIN in $DOMAINS; do
+        certbot certonly --webroot -w /ssl/wk/ --non-interactive --email "$EMAIL" --agree-tos --config-dir "$CERT_DIR" --work-dir "$WORK_DIR" -d ${DOMAIN}
+        if [ "$DEBUG" == "true" ]; then
+	           cat /var/log/letsencrypt/letsencrypt.log
+        fi
+    done
+    
     sleep 1d
 done
 
