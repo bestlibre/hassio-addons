8fc385fb1f32df629f995cae52a39c8de65423b4
BestLibre <github@bestlibre.org>
2017-05-23 11:53:24 +0200

    Fix run.sh for nginx

--- nginx_proxy/run.sh
+++ nginx_proxy/run.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -e
 
-ssl=$(cat /data/options.json | jq -r '.ssl // empty')
-if [ "$ssl" == "true" ]; then
-    tpl=vhost.tpl
-    if [ ! -e /data/dhparam.pem ]; then
-        openssl dhparam -dsaparam -out /data/dhparam.pem 4096
-    fi
-else
-    tpl=vhost_nossl.tpl
-fi
-
-cat /data/options.json | jq -r '.vhosts[] | .vhost+" "+.port+" "+if .default_server then "default_server" else "" end' | while read VHOST PORT DEFAULT_SERVER;do export VHOST PORT DEFAULT_SERVER;envsubst '$VHOST $PORT $DEFAULT_SERVER' < /templates/${tpl} >> /etc/nginx/conf.d/${VHOST}.conf;unset VHOST PORT DEFAULT_SERVER;done
+mustache-cli /data/options.json /templates/vhost.mustache >> /etc/nginx/conf.d/vhosts.conf
 
 nginx
