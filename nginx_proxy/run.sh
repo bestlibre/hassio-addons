7f02fd875604b0cbcd22100d5b7245a7ea10cee2
BestLibre <github@bestlibre.org>
2017-05-23 12:12:55 +0200

    Fix run.sh for nginx (dhparam generation was deleted)

--- nginx_proxy/run.sh
+++ nginx_proxy/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
-
-mustache-cli /data/options.json /templates/vhost.mustache >> /etc/nginx/conf.d/vhosts.conf
+ssl=$(jq -r '[.vhosts[] | .certname // empty] | if length > 0 then "true" else "false" end' /data/options.json)
+if [ "$ssl" == "true" ]; then
+    if [ ! -e /data/dhparam.pem ]; then
+        openssl dhparam -dsaparam -out /data/dhparam.pem 4096
+    fi
+fi
+mustache-cli /data/options.json /templates/vhost.mustache
 
 nginx
