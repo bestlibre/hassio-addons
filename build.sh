769609b95c0fc3bd5987f306bdb8a3d5401125cc
BestLibre <github@bestlibre.org>
2017-05-23 18:08:22 +0200

    Testing conditional build

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 
 addon=$1
 
+if [ -z $TRAVIS_COMMIT_RANGE ] || git diff --name-only $TRAVIS_COMMIT_RANGE | grep -q $addon; then
 archs=$(jq -r '.arch // ["armhf","amd64","aarch64","i386"] | .[]' ${addon}/config.json)
 
 for arch in $archs
 do
     ./create_hassio_addon.sh -a $arch -s $addon -p -l . 
 done
+else
+    echo "No change in commit range $TRAVIS_COMMIT_RANGE"
+fi
