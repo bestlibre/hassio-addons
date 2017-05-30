3f5b01aef44f90ef93283a213e464fd8958f6f97
BestLibre <github@bestlibre.org>
2017-05-30 14:30:42 +0200

    Better options handling for mopidy_cast

--- mopidy_cast/run.sh
+++ mopidy_cast/run.sh
@@ -2,11 +2,12 @@
 set -e
 
 local_scan=$(cat /data/options.json | jq -r '.local_scan // empty')
-options=$(cat /data/options.json | jq -r '[.options[] | "-o "+.name+"="+.value ] | join(" ")')
+options=$(cat /data/options.json | jq -r 'if .options then [.options[] | "-o "+.name+"="+.value ] | join(" ") else "" end')
+config="/var/lib/mopidy/.config/mopidy/mopidy.conf:/var/lib/mopidy/.config/mopidy/mopidy_cast.conf"
 
 snapserver &
 
 if  [ "$local_scan" == "true" ]; then
-    mopidy --config /var/lib/mopidy/.config/mopidy/mopidy.conf $options local scan
+    mopidy --config $config $options local scan
 fi
-mopidy --config /var/lib/mopidy/.config/mopidy/mopidy.conf:/var/lib/mopidy/.config/mopidy/mopidy_cast.conf $options
+mopidy --config $config $options
