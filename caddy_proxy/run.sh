b036599120ec7f439bffb44f94e7e349d293f573
BestLibre <github@bestlibre.org>
2017-08-17 15:08:42 +0200

    Remote ip supported

--- caddy_proxy/run.sh
+++ caddy_proxy/run.sh
@@ -3,5 +3,6 @@ set -e
 email=$(jq -r '.email' /data/options.json)
 
 mustache-cli /data/options.json /templates/vhost.mustache > /tmp/caddy.conf
+mkdir -p /ssl/caddy
 export CADDYPATH=/ssl/caddy
 caddy -agree -email "$email" -conf /tmp/caddy.conf
