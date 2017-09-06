1eb9eb2d778cf3e47a99d6630c4260212b57713c
BestLibre <github@bestlibre.org>
2017-09-06 16:05:50 +0200

    Fixing bug in syncthing/run.sh, bumping syncthing version and using pre-build binary

--- syncthing/run.sh
+++ syncthing/run.sh
@@ -4,8 +4,8 @@ set -e
 if [ ! -f '/data/config.xml' ]; then
     # Run syncthing to generate initial configuration files, then edit
     # config.xml to remove 127.0.0.1 limit from the GUI address.
-    /syncthing -generate=/data
-    sed -i 's/<address>127.0.0.1:8384<\/address>/<address>:8384<\/address>' /data/config.xml
+    syncthing -generate=/data
+    sed -i 's|<address>127.0.0.1:8384</address>|<address>:8384</address>|' /data/config.xml
 fi
 syncthing -no-browser -home=/data/
 
