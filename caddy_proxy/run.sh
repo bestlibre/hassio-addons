af4cfbf37df965de9a774e6959dedcc9e4eb9681
BestLibre <github@bestlibre.org>
2017-09-07 13:26:23 +0200

    Adding https only option to caddy

--- caddy_proxy/run.sh
+++ caddy_proxy/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 email=$(jq -r '.email' /data/options.json)
-
+https_only=$(jq -r 'if .disable_http_chalenge? then "-disable-http-challenge" else "" ' /data/options.json)
 mustache-cli /data/options.json /templates/vhost.mustache > /tmp/caddy.conf
 mkdir -p /ssl/caddy
 export CADDYPATH=/ssl/caddy
-caddy -agree -email "$email" -conf /tmp/caddy.conf
+caddy -agree ${https_only} -email "$email" -conf /tmp/caddy.conf
