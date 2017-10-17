0e53d48774a267db38be78b380af9d6ab1b1a1c0
bestlibre <github@bestlibre.org>
2017-10-17 22:47:29 +0200

    Using new build system, marking optional configration with ?

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 for addon in "$@";
 do
     if [ -z $TRAVIS_COMMIT_RANGE ] || git diff --name-only $TRAVIS_COMMIT_RANGE | grep -v README.md | grep -q $addon; then
-	archs=$(jq -r '.arch // ["armhf","amd64","aarch64","i386"] | .[]' ${addon}/config.json)
+	archs=$(jq -r '.arch // ["all"] | .[]' ${addon}/config.json)
 	for arch in $archs
 	do
-	    ./create_hassio_addon.sh -a $arch -s $addon -l .
+	    docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/$addon:/data homeassistant/amd64-builder --${arch} -t /data
 	done
     else
 	echo "No change in commit range $TRAVIS_COMMIT_RANGE"
