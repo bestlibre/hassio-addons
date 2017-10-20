0eb167363d1064e71ae0db54c3d6140d6d8ecc4b
BestLibre <github@bestlibre.org>
2017-10-20 09:40:47 +0200

    Sequential build instead of parallel for all archs. Add nextcloud to travis

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-for addon in "$@";
-do
-    if [ -z $TRAVIS_COMMIT_RANGE ] || git diff --name-only $TRAVIS_COMMIT_RANGE | grep -v README.md | grep -q $addon; then
-	archs=$(jq -r '.arch // ["all"] | .[]' ${addon}/config.json)
-	for arch in $archs
-	do
-	    docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/$addon:/data homeassistant/amd64-builder --${arch} -t /data
+for addon in "$@"; do
+    if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
+	archs=$(jq -r '.arch // ["armhf", "amd64", "aarch64", "i386"] | .[]' ${addon}/config.json)
+	for arch in $archs; do
+	    docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder --${arch} -t /data
 	done
     else
-	echo "No change in commit range $TRAVIS_COMMIT_RANGE"
+	echo "No change in commit range ${TRAVIS_COMMIT_RANGE}"
     fi
 done
