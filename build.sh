b1ac93ea52a132c918631322e709d17a5e35afda
bestlibre <github@bestlibre.org>
2017-10-21 15:31:37 +0200

    Back on parallel build

--- build.sh
+++ build.sh
@@ -2,10 +2,8 @@
 
 for addon in "$@"; do
     if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
-	archs=$(jq -r '.arch // ["armhf", "amd64", "aarch64", "i386"] | .[]' ${addon}/config.json)
-	for arch in $archs; do
-	    docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder --${arch} -t /data
-	done
+	archs=$(jq -r '.arch // ["armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ${addon}/config.json)
+        docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder ${archs} -t /data
     else
 	echo "No change in commit range ${TRAVIS_COMMIT_RANGE}"
     fi
