0931b8f39e039202d40aeedaaee1af3d06645bb9
bestlibre <github@bestlibre.org>
2017-10-21 17:02:10 +0200

    triing without cache

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 for addon in "$@"; do
     if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
 	archs=$(jq -r '.arch // ["armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ${addon}/config.json)
-        docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder ${archs} -t /data
+        docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder ${archs} -t /data --no-cache
     else
 	echo "No change in commit range ${TRAVIS_COMMIT_RANGE}"
     fi
