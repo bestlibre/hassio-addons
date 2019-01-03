ba679cc66ff104f0b8101ae1905e08ec21acf2a1
BEstLibre <github@bestlibre.org>
2019-01-03 09:02:57 +0100

    Small modif for build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ for addon in "$@"; do
   if [ -z "$archs" ]; then
     	archs=$(jq -r '.arch // ["armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ${addon}/config.json)
   fi
-        docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder ${archs} -t /data --no-cache
+        docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder ${archs} -t /data --no-cache ${TEST}
     else
 	echo "No change in commit range ${TRAVIS_COMMIT_RANGE}"
     fi
