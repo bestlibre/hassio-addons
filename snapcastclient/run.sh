89914eb128a819faea8afc2983dff76f08864c52
BEstLibre <github@bestlibre.org>
2019-05-05 14:07:41 +0200

    wip snapclient

--- snapcastclient/run.sh
+++ snapcastclient/run.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 set -e
 host=$(jq -r '.host // empty' /data/options.json)
+soundcard=$(jq -r '.soundcard // empty' /data/options.json)
+name=$(jq -r '.name // empty' /data/options.json)
 if [ -n "${host}" ];
 then
     host="-h ${host}"
 fi
-snapclient ${host} 
+if [ -n "${soundcard}" ];
+then
+    soundcard="-s ${soundcard}"
+fi
+if [ -n "${name}" ];
+then
+    name="--hostID ${name}"
+fi
+snapclient -l
+snapclient ${host} ${soundcard} ${name}
