2747477c308e3b767f19f39d04df37ff07d5d21a
BEstLibre <github@bestlibre.org>
2021-03-19 12:51:52 +0100

    Use local docker

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ for addon in "$@"; do
   if [ -z "$archs" ]; then
     	archs=$(jq -r '.arch // ["armv7", "armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ${addon}/config.json)
   fi
-        docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder ${archs} -t /data --no-cache ${TEST}
+        docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder ${archs} -t /data --no-cache ${TEST}
     else
 	echo "No change in commit range ${TRAVIS_COMMIT_RANGE}"
     fi
